8f67e1184dcf7a297162a2a8c5284315ba75228b
Ramal19 <ld4xz51vd@code.edu.az>
2024-05-11 11:23:30 +0400

    task__done

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,10 @@ export default function App() {
         <div className="menu">
           <div className="menu__left">
             <div className="btn">
-              <button className='btnOne'>Web Design</button>
-              <button className='btnTwo'>Graphics</button>
-              <button className='btnThree'>Web Coding</button>
+              <button>Web Design</button>
+              <button>Graphics</button>
+              <button>Web Coding</button>
             </div>
-            <div className="menu__left__item__btn">
               <div className="menu__left__item">
                 <span className='span__head'>Project Title</span>
                 <span className='span__head'>Budget</span>
@@ -25,33 +24,29 @@ export default function App() {
               </div>
               <div className="menu__left__item">
                 <span className='span__green'>Website Redesign</span>
-                <span className='spanNone'>$1,500 to $2,200</span>
-                <span className='spanNone'>2022 Dec 12</span>
-                <span className='spanNone'>Web Biz</span>
-                <span className='span__flex'>$1,500 to $2,200</span>
-                <span className='span__flex'>2022 Dec 12</span>
-                <span className='span__flex'>Web Biz</span>
-              </div>
-              <div className="menu__left__item">
-                <span className='span__green'>Website Renovation</span>
                 <span>$1,500 to $2,200</span>
                 <span>2022 Dec 12</span>
                 <span>Web Biz</span>
               </div>
               <div className="menu__left__item">
-                <span className='span__green'>Marketing Plan</span>
+                <span className='span__green'>Website Redesign</span>
                 <span>$1,500 to $2,200</span>
                 <span>2022 Dec 12</span>
                 <span>Web Biz</span>
               </div>
               <div className="menu__left__item">
-                <span className='span__green'>All-new Website</span>
+                <span className='span__green'>Website Redesign</span>
+                <span>$1,500 to $2,200</span>
+                <span>2022 Dec 12</span>
+                <span>Web Biz</span>
+              </div>
+              <div className="menu__left__item">
+                <span className='span__green'>Website Redesign</span>
                 <span>$1,500 to $2,200</span>
                 <span>2022 Dec 12</span>
                 <span>Web Biz</span>
               </div>
           </div>
-            </div>
           <div className="menu__right">
             <h4>Please tell us about your idea and how you want it to be</h4>
             <p>You are allowed to use this template for your websites. You are <span>NOT allowed</span> to redistribute the template ZIP file on any other template websites.
@@ -59,8 +54,38 @@ export default function App() {
               Thank you for downloading and using our templates. Please tell your friends about our website.</p>
               <button>Discover More</button>
           </div>
+          </div>
         </div>
-      </div>
+        <section>
+          <img src="https://templatemo.com/templates/templatemo_574_mexant/assets/images/calculator-image.png" />
+          <div className="containerOne">
+            <h6>YOUR FREEDOM</h6>
+            <h4>Get A Financial Plan</h4>
+            <div className="inp">
+              <div className="inp1">
+              <p>Your Name</p>
+              <input type="text"/>
+              </div>
+              <div className="inp1">
+              <p>Your Email</p>
+              <input type="text" name="" id="" />
+              </div>
+            </div>
+            <div className="inp1">
+            <p>Subject</p>
+            <input type="text" name="" id="" />
+            </div>
+            <div className="inp1">
+            <p>Your Reason</p>
+            <select name="" id="">
+              <option value="" selected>Choose an Option</option>
+              <option value="">Choose an Option</option>
+              <option value="">Choose an Option</option>
+            </select>
+            </div>
+            <button>Submit Now</button>
+          </div>
+        </section>
     </>
   )
 }
